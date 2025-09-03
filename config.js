ce8321deef160cb4733a73c79b90b6a2bce30188
sebas19956 <42702576+sebas19956@users.noreply.github.com>
2025-09-03 10:23:35 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Rellena esta URL después de desplegar tu Google Apps Script como Web App.
 const CONFIG = {
-  GAS_ENDPOINT: "" // ejemplo: "https://script.google.com/macros/s/AKfycbx.../exec"
+  GAS_ENDPOINT: "" https://script.google.com/macros/s/AKfycbzq5yZ0MYmyb4sDtHwqFyH7DnG9Baj9g-DILHNAUbEkMF1_cDhEVaJtgFwZ80Z0EIf9/exec"
 };
